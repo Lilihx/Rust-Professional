6124ed7b9146e18d58c65786694bfc5f6e1b530e
lihengxin.123 <lihengxin.123@bytedance.com>
2025-02-28 21:09:56 +0800

    feat: solve hard-2

--- exercises/hard/solutiont2/src/prime_factor.rs
+++ exercises/hard/solutiont2/src/prime_factor.rs
@@ -1,3 +1,46 @@
+fn gen_prime(limit: u128) -> Vec<u128> {
+    let mut primes: Vec<u128> = Vec::new();
+    let mut is_prime: Vec<bool> = vec![true; (limit + 1) as usize];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for i in 2..=limit {
+        if is_prime[i as usize] {
+            primes.push(i);
+            let mut j = i * i;
+            while j <= limit {
+                is_prime[j as usize] = false;
+                j += i;
+            }
+        }
+    }
+    primes
+}
+
 pub fn find_max_prime_factor(number: u128) -> u128 {
-    todo!()
+    let mut number = number;
+    let mut max_prime = 0;
+    let primes = gen_prime(10_000_000);
+    for &prime in &primes {
+        while number % prime == 0 {
+            number /= prime;
+            max_prime = prime;
+        }
+    }
+    // 求factor
+    let mut factor = *primes.last().unwrap();
+    if (number as f64).sqrt() as u128 > *primes.last().unwrap() as u128 {
+        factor = primes.last().unwrap().pow(2) as u128;
+    }
+    while factor * factor <= number {
+        while number % factor == 0 {
+            number /= factor;
+            max_prime = factor;
+        }
+        while number % (factor + 2) == 0 {
+            max_prime = factor + 2;
+            number /= factor + 2;
+        }
+        factor += 6;
+    }
+    number.max(max_prime)
 }
---
+++ exercises/hard/solutiont3/target/debug/deps/solutiont3-900265bd97177934.d
@@ -0,0 +1,6 @@
+/Users/bytedance/Documents/github/tag-course/Rust-Professional/exercises/hard/solutiont3/target/debug/deps/libsolutiont3-900265bd97177934.rmeta: src/main.rs src/district.rs
+
+/Users/bytedance/Documents/github/tag-course/Rust-Professional/exercises/hard/solutiont3/target/debug/deps/solutiont3-900265bd97177934.d: src/main.rs src/district.rs
+
+src/main.rs:
+src/district.rs:
